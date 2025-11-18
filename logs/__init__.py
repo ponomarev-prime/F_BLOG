52f72a74919a9f71928fd456038fcac74e4ac125
Aleksandr Ponomarev <ax.dev@yahoo.com>
2025-11-18 10:57:30 +0300

    Modify logs - now it use UTF-8

--- logs/__init__.py
+++ logs/__init__.py
@@ -1,11 +1,14 @@
 import logging
 from logging.handlers import RotatingFileHandler
+import os
 
 logger = logging.getLogger(__name__)
 
 # Создайте объект RotatingFileHandler для логирования в файл
-log_file = 'logs/flask_blog.log'
-handler = RotatingFileHandler(log_file, maxBytes=1024*1024, backupCount=5)  # Максимальный размер 1 МБ, хранить 5 бэкапов
+log_dir = os.path.dirname(__file__)
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, 'flask_blog.log')
+handler = RotatingFileHandler(log_file, maxBytes=1024*1024, backupCount=5, encoding='utf-8')  # Максимальный размер 1 МБ, хранить 5 бэкапов
 
 # Создайте форматтер и добавьте его к обработчику
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(pathname)s:%(lineno)d - %(funcName)s - %(message)s')
